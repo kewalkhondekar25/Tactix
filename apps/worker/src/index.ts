5bc4c95d38dae49f9175a0efc690bfe7cb8be957
Kewal Khondekar <kewalkhondekar25@gmail.com>
2025-03-27 05:45:25 +0530

    feat: worked kafka producer added

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -7,8 +7,11 @@ const main = async () => {
   
   try {
     const consumer = kafka.consumer({ groupId: "worker"});
+    const producer = kafka.producer();
     
     await consumer.connect();
+    await producer.connect();
+
     await consumer.subscribe({ topic: TOPIC_NAME, fromBeginning: true});
     await consumer.run({
       autoCommit: false,
@@ -19,7 +22,43 @@ const main = async () => {
           value: message.value?.toString()
         });
 
+        if(!message.value?.toString()) return;
+        const parsedValue = JSON.parse(message.value?.toString());
+
+        const flowRunId = parsedValue.flowRunId;
+        const stage = flowRunId.stage;
+
+        const flowRunDetails = await client.flowRun.findFirst({
+          where: { id: flowRunId },
+          include: {
+            flow: {
+              include: {
+                action: true
+              }
+            }
+          }
+        });
+
+        const currentStage = flowRunDetails?.flow.action.find(item => item.order === stage);
+
+        if(currentStage?.event === "SEND_EMAIL"){
+          //send email
+        };
+
+        if(currentStage?.event === "CREATE_DOC"){
+          //create doc
+        };
+
         await new Promise((resolve) => setTimeout(resolve, 3000));
+
+        const lastStage = flowRunDetails?.flow.action.length;
+        if(lastStage !== stage){
+          await producer.send({
+            topic: TOPIC_NAME,
+            messages: [{ value: JSON.stringify({ flowRunId, stage: stage + 1 })}]
+          })
+        }
+
         console.log("processing!!!");
         
         await consumer.commitOffsets([{
